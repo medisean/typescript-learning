d5942bf35216f7a93023c751c80736ba9dabcf4e
zouliangming <liangmingzou@163.com>
2020-01-02 11:36:52 +0800

    update class demo

--- string/ClassDemo.js
+++ string/ClassDemo.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = /** @class */ (function () {
     function Person(name) {
         this.name = name;
@@ -7,6 +20,20 @@ var Person = /** @class */ (function () {
     };
     return Person;
 }());
-// let p: Person = new Person("person");
-var p = new Person();
+var p = new Person("person");
 p.disp();
+var Student = /** @class */ (function (_super) {
+    __extends(Student, _super);
+    function Student(name, id) {
+        var _this = _super.call(this, name) || this;
+        _this.id = id;
+        return _this;
+    }
+    Student.prototype.disp = function () {
+        _super.prototype.disp.call(this);
+        console.log(this.id);
+    };
+    return Student;
+}(Person));
+var s = new Student("John", "123");
+s.disp();
--- string/ClassDemo.ts
+++ string/ClassDemo.ts
@@ -12,3 +12,19 @@ class Person {
 
 let p: Person = new Person("person");
 p.disp();
+
+class Student extends Person {
+  id: string;
+  constructor(name: string, id: string) {
+    super(name)
+    this.id = id
+  }
+
+  disp(): void {
+    super.disp();
+    console.log(this.id);
+  }
+}
+
+let s: Student = new Student("John", "123");
+s.disp();
